26a226f69958d5868d8c7e704819c92696ee2db1
SergH1980 <112980777+SergH1980@users.noreply.github.com>
2023-03-06 14:22:00 +0000

    Deploying to gh-pages from @ SergH1980/goit-js-hw-09@684399b6fbe1b698c68e2a21eb4bffb813fb3eec 🚀

--- 03-promises.2ffc9325.js
+++
@@ -1,2 +0,0 @@
-({form:document.querySelector(".form"),button:document.querySelector("button")}).form.addEventListener("submit",(function(e){e.preventDefault(),console.log(e.currentTarget.elements.delay.value),console.log(e.currentTarget.elements.step.value),console.log(e.currentTarget.elements.amount.value)}));
-//# sourceMappingURL=03-promises.2ffc9325.js.map
--- 03-promises.8215e078.js
+++
@@ -1,2 +0,0 @@
-[document.querySelector(".form"),document.querySelector("button")][0].addEventListener("submit",(function(e){e.preventDefault(),console.log(e.currentTarget.elements.delay.value),console.log(e.currentTarget.elements.step.value),console.log(e.currentTarget.elements.amount.value)}));
-//# sourceMappingURL=03-promises.8215e078.js.map
